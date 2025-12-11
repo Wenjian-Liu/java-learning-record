acc45abe381425a582d3c6453130a43be5d7a1fa
Wenjian-Liu <artshirly@126.com>
2025-12-11 23:08:33 +0800

    feat: add constructor demo and salary calculation example
    - Added Employee class with constructor, getters/setters, and salary calculation logic
    - Added SalaryDemo to demonstrate object creation, method calling, and return flow
    - Practiced constructor use, method decomposition, and return value tracing

--- code/oop/SalaryDemo.java
+++ code/oop/SalaryDemo.java
@@ -2,11 +2,7 @@ package com.itheima.chap02;
 
 public class SalaryDemo {
     public static void main(String[] args) {
-        Employee employee = new Employee();
-        employee.setBaseSalary(5000);
-        System.out.println(employee.getBaseSalary());
-        employee.setBonus(10000);
-
+        Employee employee = new Employee(5000,10000);
         int salary =employee.calculateSalary('C');
         System.out.println(salary);
     }
