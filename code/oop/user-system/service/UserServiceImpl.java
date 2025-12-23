086ca9635a90c5b5af2f2aefd27f7d1f304b4c73
Wenjian-Liu <artshirly@126.com>
2025-12-23 22:31:13 +0800

    refactor: decouple user-system by extracting responsibilities
    - Extracted UI-related logic into dedicated classes
    - Clarified responsibility boundaries across layers
    - Improved main entry structure for dependency assembly

---
+++ code/oop/user-system/service/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package com.itheima.users.service;
+
+import com.itheima.chap04.Result;
+import com.itheima.users.dao.UserDatabase;
+import com.itheima.users.entity.User;
+
+public class UserServiceImpl implements UserService {
+
+    private UserDatabase userDB;
+
+    public UserServiceImpl(UserDatabase userDB) {
+        this.userDB = userDB;
+    }
+
+    @Override
+    public Result register(String username, String password) {
+        // 校验数据信息
+        // 校验用户名
+        if(isEmptyString(username)) {
+            //用户名有误
+            return Result.fail("用户名不能为空");
+        }
+        // 校验密码
+        if(isEmptyString(password)) {
+            return Result.fail("密码不能为空");
+        }
+        // 校验用户名是否存在
+        User user = userDB.queryByUsername(username);
+        if(user != null) {
+            return Result.fail("用户名已存在！");
+        }
+        // 保存用户信息
+        user = new User(username, password);
+        userDB.save(user);
+        return Result.ok(user);
+    }
+
+    @Override
+    public Result login(String username, String password) {
+        // 校验基本信息
+        if(isEmptyString(username) || isEmptyString(password)) {
+            //用户名有误
+            return Result.fail("用户名或密码不能为空");
+        }
+        // 根据用户名查询用户
+        User user = userDB.queryByUsername(username);
+        // 判断用户名是否存在
+        if(user == null) {
+            return Result.fail("用户名不存在");
+        }
+        // 判断密码是否正确
+        if(!password.equals(user.getPassword())) {
+            return Result.fail("密码不正确");
+        }
+        return Result.ok(user);
+    }
+
+    private boolean isEmptyString(String str) {
+        return str == null || str.equals("");
+    }
+}
